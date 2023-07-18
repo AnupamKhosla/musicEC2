f233989ca2cf5e3b331563ef20b4a7ed714bdc2b
Anon <anupamkhosla1993@gmail.com>
2023-07-18 20:56:27 +0530

    debugging db connection

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 import express from 'express';
 import path from 'path';
 import cors from "cors";
-//import "./loadEnvironment.mjs";
+import "./loadEnvironment.mjs";
 //import "express-async-errors";
 //import posts from "./routes/posts.mjs";
-//import db from "./db/conn.mjs";
-
+import db from "./db/conn.mjs";
+console.log(db);
+throw new Error("Test Error");
 
 var port = process.env.PORT || 3000;
 var app = express();
